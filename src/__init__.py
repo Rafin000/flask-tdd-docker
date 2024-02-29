53d15a3786531a83d4a4f4d34be39c0a074eb68f
Rafin000 <marufulislam00000@gmail.com>
2024-02-29 11:55:54 +0600

    Part One Full Complete

--- src/__init__.py
+++ src/__init__.py
@@ -1,38 +1,37 @@
-from flask import Flask
+
 import os
-from flask_restx import Resource, Api
-import sys
+
+from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
-api= Api(app)
-
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings) 
-
-db = SQLAlchemy(app)
+# instantiate the db
+db = SQLAlchemy()
 
 
-class User(db.Model):  
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+# new
+def create_app(script_info=None):
     
+    # instantiate the app
+    app = Flask(__name__)
 
-api.add_resource(Ping, '/ping')
-print(app.config, file=sys.stderr)
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    from src.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
